4c44e10f42c8384f81485a8d36d2a978cb2e2fe3
VinhLB <dongoc409@gmail.com>
2023-03-20 06:38:02 +0700

    #19.7 Demo transactional method inner method process task in JdbcTemplate

---
+++ SpringFramework/src/main/java/spring/springframework/springdata/InnerTransactionMethod/EmployeeService.java
@@ -0,0 +1,29 @@
+package spring.springframework.springdata.InnerTransactionMethod;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EmployeeService {
+
+    public void transaction1And2() {
+        transaction1();
+        transaction2();
+    }
+
+    @Transactional
+    public void transaction1And2WithPrecedingTransaction() {
+        transaction1();
+        transaction2();
+    }
+
+    @Transactional
+    public void transaction1() {
+        System.out.println("transaction1 method called");
+    }
+
+    @Transactional
+    public void transaction2() {
+        System.out.println("transaction2 method called");
+    }
+}
--- SpringFramework/src/main/java/spring/springframework/springdata/TransactionalJdbcTemplate/EmployeesService.java
+++ SpringFramework/src/main/java/spring/springframework/springdata/TransactionalJdbcTemplate/EmployeesService.java
@@ -2,6 +2,7 @@ package spring.springframework.springdata.TransactionalJdbcTemplate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
